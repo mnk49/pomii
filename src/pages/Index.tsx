1262436e59828c6ad3b7c1184e8c2e5b15f7847e
[dyad] <gb_361810@akesp.net>
2025-08-03 19:40:22 +0500

    [dyad] Add animated mesh background - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,9 +3,16 @@ import { MadeWithDyad } from "@/components/made-with-dyad";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 dark:from-gray-900 dark:to-slate-800 flex flex-col items-center justify-center p-4">
-      <PomodoroTimer />
-      <div className="absolute bottom-4">
+    <div className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 dark:from-gray-900 dark:to-slate-800 flex flex-col items-center justify-center p-4 relative overflow-hidden">
+      <div className="animated-mesh-gradient-container">
+        <div className="blob-1"></div>
+        <div className="blob-2"></div>
+        <div className="blob-3"></div>
+      </div>
+      <div className="relative z-10">
+        <PomodoroTimer />
+      </div>
+      <div className="absolute bottom-4 z-10">
         <MadeWithDyad />
       </div>
     </div>

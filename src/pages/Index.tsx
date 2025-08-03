66dedcae3857a9c42851049a6695c81dd1a345a3
[dyad] <gb_361810@akesp.net>
2025-08-03 20:02:35 +0500

    [dyad] Replace CSS background with Vanta.js - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,12 +3,7 @@ import { MadeWithDyad } from "@/components/made-with-dyad";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 dark:from-gray-900 dark:to-slate-800 flex flex-col items-center justify-center p-4 relative overflow-hidden toy-pattern-bg">
-      <div className="animated-mesh-gradient-container">
-        <div className="blob-1"></div>
-        <div className="blob-2"></div>
-        <div className="blob-3"></div>
-      </div>
+    <div className="min-h-screen flex flex-col items-center justify-center p-4 relative">
       <div className="relative z-10">
         <PomodoroTimer />
       </div>

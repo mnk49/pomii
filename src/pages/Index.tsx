cdcacc8c786ba6db0b433203f5a641772d37f4cd
[dyad] <gb_361810@akesp.net>
2025-08-05 22:04:40 +0500

    [dyad] Adjust Pomodoro timer width - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import { MadeWithDyad } from "@/components/made-with-dyad";
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4 relative">
-      <div className="relative z-10 w-full">
+      <div className="relative z-10 w-full max-w-4xl">
         <PomodoroTimer />
       </div>
       <div className="absolute bottom-4 z-10">
